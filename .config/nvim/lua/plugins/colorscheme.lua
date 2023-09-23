35896c3261e39a76b3f2183dd20207a45575d0bd
alexrf45 <fonalex45@gmail.com>
2023-09-23 11:04:25 -0400

    dracula ftw

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-  "tiagovla/tokyodark.nvim",
-  opts = {
-    transparent_background = "true",
-    terminal_colors = true,
-  },
-  config = function(_, opts)
-    require("tokyodark").setup(opts) -- calling setup is optional
-    vim.cmd([[colorscheme tokyodark]])
+  "maxmx03/dracula.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local dracula = require("dracula")
+
+    dracula.setup()
+
+    vim.cmd.colorscheme("dracula")
   end,
 }
